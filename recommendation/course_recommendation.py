fddfc9f0793315300b10e132dacf35d6b653f045
Jason Xu <ruiyang.xu18@imperial.ac.uk>
2019-11-02 07:06:41 +0000

    University recommendation and input and output

--- recommendation/course_recommendation.py
+++ recommendation/course_recommendation.py
@@ -14,7 +14,7 @@ colOffset = dataset.shape[1]
 for i in range(len(aLevels)):
   dataset.insert(colOffset + i, aLevels[i], 0)
 
-print(dataset.head())
+#print(dataset.head())
 
 aLevelColNames = ['a_level_1', 'a_level_2', 'a_level_3', 'a_level_4']
 for i in range(dataset.shape[0]):
@@ -29,7 +29,7 @@ for i in range(dataset.shape[0]):
   course = dataset.at[i, 'course']
   dataset.at[i, "Taking " + course] = 1
 
-print(dataset.head())
+#print(dataset.head())
 
 regressors = {}
 
@@ -44,7 +44,6 @@ for i in range(len(courses)):
   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
   # Feature Scaling
-
   sc = StandardScaler()
   X_train = sc.fit_transform(X_train)
   X_test = sc.transform(X_test)
@@ -57,18 +56,20 @@ for i in range(len(courses)):
 
   y_pred = regressor.predict(X_test)
 
-
-  #print(confusion_matrix(y_test,y_pred))
-  #print(classification_report(y_test,y_pred))
-  #print(accuracy_score(y_test, y_pred))
-
-aLevelsTaking2 = ['Physics', 'Chemistry', 'Maths', "Economics"]
+aLevelsTaking2 = ARG # ['Physics', 'Chemistry', 'Maths', "Music"]
 aLevelsTaking2 = map(lambda x: aLevels.index(x), aLevelsTaking2)
 
 aLevelsTaking = [0] * len(aLevels)
 for course in aLevelsTaking2:
   aLevelsTaking[course] = 1
 
-for course in courses:
-  print(course + ": "+ str(regressors[course].predict([aLevelsTaking])[0]))
+recommendation = []
 
+for course in courses:
+  result = (course, regressors[course].predict([aLevelsTaking])[0])
+  recommendation.append(result)
+
+recommendation.sort(key = lambda x: x[1], reverse = True)
+recommendation = list(map(lambda x: x[0], recommendation))
+
+print(recommendation[:3])
---
+++ recommendation/uni_recommendation.py
@@ -0,0 +1,21 @@
+grade_Converter = {"A*" : 56,
+                   "A" : 48,
+                   "B" : 40,
+                   "C" : 32}
+
+uni_Requirements = [("University of Cambridge", 160),
+                    ("Imperial College London", 152),
+                    ("University of Oxford", 152),
+                    ("Durham University", 152), 
+                    ("University College London", 144),
+                    ("King's College London", 144),
+                    ("University of Warwick", 144),
+                    ("London School of Economics and Political Science", 136)]
+
+predicted_Grades = ARG
+
+ucas_Score = sum(map(lambda x: grade_Converter[x], predicted_Grades))
+
+valid_Unis = list(map(lambda x: x[0], (filter(lambda x: x[1] <= ucas_Score, uni_Requirements))))
+
+print(valid_Unis)
